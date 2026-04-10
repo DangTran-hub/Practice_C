8b4000e811ef4d7d4102f6ccbed4f43c7479d1a4
Dang Tran <dangtranmyha03092003@gmail.com>
2026-04-10 15:03:44 +0700

    create exercise 3

---
+++ ex3/ex3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+int main()
+{
+    int STATE = OUT;
+    int c;
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ' || c == '\t' || c == '\n')
+        {
+            STATE = OUT;
+        }
+        else
+        {
+            if (STATE == OUT)
+            {
+                STATE = IN;
+                putchar('\n');
+            }
+            putchar(c);
+        }
+    }
+
+    return 0;
+}
